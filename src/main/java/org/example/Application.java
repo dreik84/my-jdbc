9f17340007d2d7a91f727bf4e33440a94c7959a9
dreik84 <dreik84@mail.ru>
2026-01-27 19:34:17 +0300

    Update Application

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -4,11 +4,43 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Application {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException {
 
 //        Class.forName("org.h2.Driver");
 
         var connection = DriverManager.getConnection("jdbc:h2:mem:testDB");
 
+        var sql = """
+                CREATE TABLE users (
+                    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                    username VARCHAR(255),
+                    phone VARCHAR(255)
+                );
+                """;
+
+        var statement = connection.createStatement();
+        statement.execute(sql);
+        statement.close();
+
+        sql = """
+                INSERT INTO users (username, phone) VALUES ('tommy', '1234');
+                """;
+
+        var statement2 = connection.createStatement();
+        statement2.executeUpdate(sql);
+        statement2.closeOnCompletion();
+
+        sql = "SELECT * FROM users";
+
+        var statement3 = connection.createStatement();
+        var resultSet = statement3.executeQuery(sql);
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("username"));
+            System.err.println(resultSet.getString("phone"));
+        }
+
+        statement3.close();
+        connection.close();
     }
 }
