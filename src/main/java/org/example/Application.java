f4dc6556622ef6b1bbc3c87c244884e282b30803
dreik84 <dreik84@mail.ru>
2026-01-28 18:44:36 +0300

    Update Application

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -8,39 +8,37 @@ public class Application {
 
 //        Class.forName("org.h2.Driver");
 
-        var connection = DriverManager.getConnection("jdbc:h2:mem:testDB");
+        try (var connection = DriverManager.getConnection("jdbc:h2:mem:testDB")) {
+            var sql = """
+                    CREATE TABLE users (
+                        id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                        username VARCHAR(255),
+                        phone VARCHAR(255)
+                    );
+                    """;
 
-        var sql = """
-                CREATE TABLE users (
-                    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-                    username VARCHAR(255),
-                    phone VARCHAR(255)
-                );
-                """;
+            try (var statement = connection.createStatement()) {
+                statement.execute(sql);
+            }
 
-        var statement = connection.createStatement();
-        statement.execute(sql);
-        statement.close();
+            sql = """
+                    INSERT INTO users (username, phone) VALUES ('tommy', '1234');
+                    """;
 
-        sql = """
-                INSERT INTO users (username, phone) VALUES ('tommy', '1234');
-                """;
+            try (var statement2 = connection.createStatement()) {
+                statement2.executeUpdate(sql);
+            }
 
-        var statement2 = connection.createStatement();
-        statement2.executeUpdate(sql);
-        statement2.closeOnCompletion();
+            sql = "SELECT * FROM users";
 
-        sql = "SELECT * FROM users";
+            try (var statement3 = connection.createStatement()) {
+                var resultSet = statement3.executeQuery(sql);
 
-        var statement3 = connection.createStatement();
-        var resultSet = statement3.executeQuery(sql);
-
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("username"));
-            System.err.println(resultSet.getString("phone"));
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("username"));
+                    System.err.println(resultSet.getString("phone"));
+                }
+            }
         }
-
-        statement3.close();
-        connection.close();
     }
 }
