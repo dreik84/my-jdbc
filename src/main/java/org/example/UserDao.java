0d11e39a44cf58b2541704051539651b3b5c2234
dreik84 <dreik84@mail.ru>
2026-01-30 17:02:40 +0300

    Create User dao

---
+++ src/main/java/org/example/UserDao.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class UserDao {
+    private Connection connection;
+
+    public UserDao(Connection conn) {
+        connection = conn;
+    }
+
+    public void save(User user) throws SQLException {
+        // insert
+        if (user.getId() == null) {
+            var sql = "INSERT INTO users (username, phone) VALUES (?, ?)";
+
+            try (var prepareStatement = connection.prepareStatement(sql)) {
+                prepareStatement.setString(1, user.getName());
+                prepareStatement.setString(2, user.getPhone());
+                prepareStatement.executeUpdate();
+
+                var generatedKey = prepareStatement.getGeneratedKeys();
+
+                if (generatedKey.next()) {
+                    user.setId(generatedKey.getLong("id"));
+                } else {
+                    throw new SQLException("DB have not returned a id after saving");
+                }
+            }
+        } else {
+            // TODO update
+        }
+    }
+}
