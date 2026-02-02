114d23e72ab399ee76a949ce393d800ff8f64df2
dreik84 <dreik84@mail.ru>
2026-02-02 19:31:23 +0300

    Update JdbcRunner

--- src/main/java/org/example/JdbcRunner.java
+++ src/main/java/org/example/JdbcRunner.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import com.github.javafaker.Faker;
 import org.example.util.ConnectionManager;
 
 import java.sql.Connection;
@@ -7,9 +8,27 @@ import java.sql.SQLException;
 
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
+        Faker faker = Faker.instance();
 
-        try (Connection connection = ConnectionManager.open()) {
-            System.out.println(connection.getTransactionIsolation());
+        String sql = """
+                DROP TABLE IF EXISTS test;
+                
+                CREATE TABLE IF NOT EXISTS test (
+                  id SERIAL PRIMARY KEY,
+                  data VARCHAR(256)
+                );
+                
+                INSERT INTO test (data)
+                VALUES 
+                    ('TEXT %s'),
+                    ('TEXT %s'),
+                    ('TEXT %s'),
+                    ('TEXT %s')
+                """.formatted(faker.animal().name(), faker.artist().name(), faker.book().title(), faker.color().name());
+
+        try (Connection connection = ConnectionManager.open();
+             var statement = connection.createStatement()) {
+            System.out.println(statement.execute(sql));
         }
     }
 }
