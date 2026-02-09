23938b616beee3e5a3b3bab854528c07c7a2056e
dreik84 <dreik84@mail.ru>
2026-02-09 19:41:48 +0300

    Update JdbcRunner

--- src/main/java/org/example/JdbcRunner.java
+++ src/main/java/org/example/JdbcRunner.java
@@ -1,42 +1,22 @@
 package org.example;
 
-import com.github.javafaker.Faker;
-import org.example.util.ConnectionManager;
+import org.example.dao.TicketDao;
+import org.example.entity.Ticket;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.Date;
+import java.math.BigDecimal;
 import java.sql.SQLException;
-import java.time.LocalDate;
 
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
-        Faker faker = Faker.instance();
+        TicketDao ticketDao = TicketDao.getInstance();
+        Ticket ticket = new Ticket();
+        ticket.setPassportNo("AR3434");
+        ticket.setPassengerName("John");
+        ticket.setFlightId(12L);
+        ticket.setSeatNo(45);
+        ticket.setCost(BigDecimal.TEN);
 
-        String sql = """
-                SELECT flight.flight_no
-                FROM flight 
-                WHERE arrival_date 
-                BETWEEN ? AND ?
-                """;
-
-        try (Connection connection = ConnectionManager.get();
-             var statement = connection.prepareStatement(sql)) {
-
-            statement.setFetchSize(2);
-            statement.setMaxRows(2);
-            statement.setQueryTimeout(1);
-
-            DatabaseMetaData metaData = connection.getMetaData();
-            System.out.println(metaData);
-
-
-            statement.setDate(1, Date.valueOf(LocalDate.now().minusDays(5)));
-            statement.setDate(2, Date.valueOf(LocalDate.now().plusDays(2)));
-            var resultSet = statement.executeQuery();
-
-            while (resultSet.next())
-                System.out.println(resultSet.getString("flight_no"));
-        }
+//        System.out.println(ticketDao.save(ticket));
+        System.out.println(ticketDao.delete(11L));
     }
 }
