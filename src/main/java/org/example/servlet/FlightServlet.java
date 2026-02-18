955434c2bd74144b08c7ba54e85ae51acebb35f7
dreik84 <dreik84@mail.ru>
2026-02-18 19:35:43 +0300

    Update FlightServlet

--- src/main/java/org/example/servlet/FlightServlet.java
+++ src/main/java/org/example/servlet/FlightServlet.java
@@ -1,17 +1,43 @@
 package org.example.servlet;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 
+@WebServlet("/flight")
 public class FlightServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("INIT");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet FlightServlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Servlet FlightServlet</h1>");
+        out.println("</body>");
+        out.println("</html>");
+
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("DESTROY");
     }
 }
