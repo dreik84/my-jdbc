1fac956bcdc857ea29a43fbeb0f054bf5644a104
dreik84 <dreik84@mail.ru>
2026-02-22 19:30:14 +0300

    Update CookiesServlet

---
+++ src/main/java/org/example/servlet/DispatcherServlet.java
@@ -0,0 +1,21 @@
+package org.example.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/dispatcher")
+public class DispatcherServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        var dispatcher = req.getRequestDispatcher("/flights");
+//        dispatcher.forward(req, resp);
+//        dispatcher.include(req, resp);
+        resp.sendRedirect("/flights");
+    }
+}
