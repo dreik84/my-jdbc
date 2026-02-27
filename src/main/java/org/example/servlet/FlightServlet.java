dcb484b2aa4e1948b9ce40d7e4d0055f7cef0e93
dreik84 <dreik84@mail.ru>
2026-02-27 18:49:46 +0300

    Update JSP

--- src/main/java/org/example/servlet/FlightServlet.java
+++ src/main/java/org/example/servlet/FlightServlet.java
@@ -6,9 +6,9 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.service.FlightService;
+import org.example.util.JspHelper;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 
 @WebServlet("/flights")
@@ -24,18 +24,8 @@ public class FlightServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
-
-        try (PrintWriter writer = resp.getWriter()) {
-            writer.write("<h1>Список перелётов<h1>");
-            writer.write("<ul>");
-            flightService.findAll().forEach(flightDto -> {
-                writer.write("""
-                        <li>
-                            <a href='/tickets?flightId=%d'>%s</a>
-                        </li>
-                        """.formatted(flightDto.id(), flightDto.description()));
-            });
-        }
+        req.setAttribute("flights", flightService.findAll());
+        req.getRequestDispatcher(JspHelper.getPath("flights")).forward(req, resp);
     }
 
     @Override
---
+++ src/main/webapp/WEB-INF/jsp/flights.jsp
@@ -0,0 +1,20 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Список перелетов: </h1>
+<ul>
+    <c:if test="${not empty requestScope.flights}">
+        <c:forEach var="flight" items="${requestScope.flights}">
+            <li>
+                <a href="${pageContext.request.contextPath}/tickets?flightId=${flight.id()}">${flight.description()}</a>
+            </li>
+        </c:forEach>
+    </c:if>
+</ul>
+</body>
+</html>
